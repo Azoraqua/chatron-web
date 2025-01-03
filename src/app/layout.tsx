9d68f2d1b9a29a3df81047fe4c736f3713850959
Azoraqua <info@azoraqua.dev>
2025-01-03 10:22:39 +0100

    Updated.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-});
 
 export const metadata: Metadata = {
   title: 'Chatron'
@@ -20,6 +11,9 @@ const RootLayout: FCC = ({ children }) => (
   <html lang="en">
     <body className={`antialiased`}>
       {children}
+
+      <Analytics />
+      <SpeedInsights />
     </body>
   </html>
 );
